ce4b5b71345208ea6dd173ff3821f4c08b08bb3c
unknown <Saitharala@gmail.com>
2021-12-03 00:07:46 +0530

    [ Tharala SaiPrashanth] Added  Ability to Analyse  Feauture

---
+++ MoodAnalyzerProgram/MoodAnalyzerProgram/MoodAnalyzerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerTesting
+{
+    class MoodAnalyzerTest
+    {
+        string message;
+        public MoodAnalyzerTest(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood()
+        {
+            if (message.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
---
+++ MoodAnalyzerProgram/MoodAnalyzerProgram/UnitTest1.cs
@@ -0,0 +1,25 @@
+using MoodAnalyzerTesting;
+using NUnit.Framework;
+
+namespace MoodAnalyzerProgram
+{
+        public class Tests
+        {
+            [Test]
+            public void GivenSadMessage_WhenAnalyze_ShouldReturnSad()
+            {
+                // string message="I am in Sad Mood";
+                MoodAnalyzerTest mood = new MoodAnalyzerTest("I am in Sad Mood");
+                string result = mood.AnalyzeMood();
+                Assert.AreEqual(result, "Sad");
+            }
+            [Test]
+            public void GivenHappyMessage_WhenAnalyze_ShouldReturnHappy()
+            {
+                //string message = "I am in Happy Mood";
+                MoodAnalyzerTest mood = new MoodAnalyzerTest("I am in Happy Mood");
+                string result = mood.AnalyzeMood();
+                Assert.AreEqual(result, "Happy");
+            }
+        }
+    }
